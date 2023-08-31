12d7d576b183db6bc2b934818bcc327e9689723c
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-31 09:40:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
 const canvas = document.querySelector("canvas");
-const context = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-let isDrawing = false;
-let lastX = 0;
-let lastY = 0;
-
-context.lineJoin = "round";
-context.lineCap = "round";
-context.lineWidth = 5;
-
-canvas.addEventListener("mousedown", (e) => {
-  isDrawing = true;
-  [lastX, lastY] = [
-    e.clientX - canvas.offsetLeft,
-    e.clientY - canvas.offsetTop,
